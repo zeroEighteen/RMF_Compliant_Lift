c941bf7b37b45c475fd7266c19e6eae98f1366ed
Koh Yi Lei Elliot <60764315+zeroEighteen@users.noreply.github.com>
2023-12-01 06:49:12 +0000

    feat(Add Lift Queue) fix(repeat readings of 1 QR)

--- lift_adapter_template/lift_adapter_template/lift_adapter_sim.py
+++ lift_adapter_template/lift_adapter_template/lift_adapter_sim.py
@@ -1,8 +1,13 @@
 # Set up MQTT Server
 import paho.mqtt.client as mqtt # Using Paho MQTT Python client
 import time
+import copy
 class LiftSim():
-    def __init__(self, IP):
+    def __init__(self, IP):  
+        # Declare IP address and port of MQTT broker
+        self.IP_ADDRESS = IP
+        self.PORT = 1883
+
         # States connection state with the MQTT server
         # False = Disconnected, True = Connected
         self._isConnected = False
@@ -21,17 +26,39 @@ class LiftSim():
         }
 
         self._lift_request = {
-            "lift_name": "test_lift",
-            "session_id": "1234",
-            "request_type": "1",
-            "request_floor": "1", 
-            "destination_floor": "0", 
-            "door_state": 2
+            "lift_id": "test_lift",
+            "request_id": "123456",
+            "request_level": None, 
+            "destination_level": None, 
         }
 
-    # Declare IP address and port of MQTT broker
-        self.IP_ADDRESS = IP
-        self.PORT = 1883
+        # Parallel arrays
+        # Queue contains the list of IDs of lift requests
+        # List contains all the other info, including the request ID
+        self._lift_requests_queue = []
+        self._lift_requests_list = []
+
+    def get_lift_requests_list(self):
+        return self._lift_requests_queue
+
+    def lift_queue_is_empty(self):
+        if self._lift_requests_queue == []:
+            return True
+        else: 
+            return False
+
+    
+    def resolve_lift_request(self, request_id):
+        # Resolving lift request means to 
+        #   1) Remove the ID from the queue
+        #   2) Remove the associated data from the list
+
+        queueIndex = self._lift_requests_queue.index(request_id)
+        self._lift_requests_queue.remove(queueIndex)
+        self._lift_requests_list.remove(queueIndex)
+        print(f"Lift Request ID {request_id} resolved")
+
+
 
     # The following functions are created for the Paho MQTT Client ---------------------------
 
@@ -53,6 +80,14 @@ class LiftSim():
         mqttClient.subscribe("lift_control/interface/current_lift_state")
 
         print(f"Subscribed to topics.")
+
+    def createNewLiftRequest(self, request_id, request_level, destination_level):
+        new = self._lift_request.copy.deepcopy()
+        new["request_id"] = request_id
+        new["request_level"] = request_level
+        new["destination_level"] = destination_level
+
+        return new
 
     # The following callback methods will be called as part of the template function calls
     def get_lift_sim_current_level(self, client, userdata, msg):
@@ -79,31 +114,27 @@ class LiftSim():
     def publish_lift_requests_to_lift_sim(self, level, mqttClient):
         try:
             pubMsg = mqttClient.publish(topic="lift_sim/button_pressed", payload=level.encode('utf-8'), qos=0)
-            print("pubMsg set")
             #Library methods, idk what they do
             pubMsg.wait_for_publish()
-            print("wait for publish complete")
-            print(pubMsg.is_published())
 
         except Exception as e:
             print(f"Error {e} ")
 
-    def get_lift_requests_destination_floor(self, client, userdata, msg):
+    def update_lift_request_state(self, client, userdata, msg):
         try:
             info = msg.payload.decode("utf-8")
-            print(f"State  update received: {info}")
             allInfo =  info.split(";")
-            print(f"Going form {allInfo[2]} to {allInfo[3]}")
-            self._lift_request["destination_floor"] = str(allInfo[3])
-            self._lift_request["request_floor"] = str(allInfo[2])
-            print("variables set")
+            if allInfo[1] not in self._lift_requests_queue:
+                newLiftRequest = createNewLiftRequest(allInfo[1], allInfo[2], allInfo[3])
+                self._lift_requests_list.append(newLiftRequest)
+                print(f"New Request queued. \nID: {allInfo{1}} \nRequest Floor: {allInfo[2]} \nDestination Floor:{allInfo[3]}")
         except Exception as e:
             print(f"Error {e}")
     
     def attachCallbackFunctions(self):
         mqttClient.message_callback_add("lift_sim/curr_level", self.get_lift_sim_current_level)
         mqttClient.message_callback_add("lift_sim/door_state", self.get_lift_sim_door_state)
-        mqttClient.message_callback_add("lift_control/interface/current_lift_state", self.get_lift_requests_destination_floor)
+        mqttClient.message_callback_add("lift_control/interface/current_lift_state", self.update_lift_request_state)
         
     # ----------------------------
     def check_connection(self) -> bool:
@@ -115,16 +146,17 @@ class LiftSim():
 mqttClient = mqtt.Client("lift_adapter")
 simulator = LiftSim("192.168.18.3")
 
-def on_publish(client, userdata, result):
-    print("datapbulished: ")
-    pass
-
-
+def publish_lift_state_update():
+    requestData = simulator.get_lift_requests_list()
+    simulator.publish_lift_requests_to_lift_sim(simulator.requestData["request_level"], mqttClient)
+    simulator.publish_lift_requests_to_lift_sim(simulator.request["destination_level"], mqttClient)
+    simulator.resolve_lift_request(requestData["request_id"])
+    print("Message successfully published. Lift request queue updated.")
+    
 # Run set-up Commands
 mqttClient.on_connect = simulator.on_connected
 mqttClient.on_disconnect = simulator.on_disconnected
 simulator.attachCallbackFunctions()
-mqttClient.on_publish = on_publish
 mqttClient.connect(simulator.IP_ADDRESS, simulator.PORT)
 # STart a new loop
 mqttClient.loop_start()
@@ -132,13 +164,8 @@ simulator.subscribeToTopics(mqttClient)
 while True:
     time.sleep(2)
     print("loop")
-    if simulator._lift_request["request_floor"] != None:
-        simulator.publish_lift_requests_to_lift_sim(simulator._lift_request["request_floor"], mqttClient)
-        print("one sent")
-        simulator.publish_lift_requests_to_lift_sim(simulator._lift_request["request_floor"], mqttClient)
-        print("two  sent")
-        simulator._lift_request["request_floor"] = None
-        simulator._lift_request["destination_floor"] = None
+    if simulator.lift_queue_is_empty() == False:
+        publish_lift_state_update()
     if simulator.check_connection() == False:
         print("attempting to reconnect")
    #  simulator.publish_lift_requests_to_lift_sim("7", mqttClient)
